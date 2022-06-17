6a26eee1eb097bc7a8d6b934d05c3ba345f3d136
Yuya Satake <sata.yu.7878@gmail.com>
2022-06-16 23:14:10 -0700

    reduce scanner instance

--- Lab4/src/App.java
+++ Lab4/src/App.java
@@ -4,23 +4,19 @@ import caluclator.Caluclator;
 
 public class App {
     public static void main(String[] args) throws Exception {
-
         boolean wantToContinue = true;
-
+        Scanner s = new Scanner(System.in);
         while (wantToContinue) {
             System.out.println("Please type the first number: ");
-            Scanner s1 = new Scanner(System.in);
-            double firstNum = s1.nextDouble();
-            Scanner s2 = new Scanner(System.in);
+            double firstNum = s.nextDouble();
             System.out.println("Choose an operation: ");
             System.out.println(" > Type + for addition");
             System.out.println(" > Type - for substract");
             System.out.println(" > Type * for multiply");
             System.out.println(" > Type / for divide");
-            String operator = s2.nextLine();
-            Scanner s3 = new Scanner(System.in);
+            String operator = s.next();
             System.out.println("Please type second number:");
-            double secondNum = s3.nextDouble();
+            double secondNum = s.nextDouble();
             Caluclator caluclator = new Caluclator(firstNum, secondNum);
 
             switch (operator) {
@@ -41,19 +37,10 @@ public class App {
                     System.out.println(caluclator.divide());
                     break;
             }
-            Scanner s4 = new Scanner(System.in);
-            System.out.println("do you want to continue or quit?");
-            String continueOrQuit = s4.nextLine();
+            System.out.println("Do you want to continue or quit?");
+            String continueOrQuit = s.next();
             wantToContinue = (continueOrQuit.equals("continue")) ? true : false;
-            if (!wantToContinue) {
-                s1.close();
-                s2.close();
-                s3.close();
-                s4.close();
-            }
         }
+        s.close();
     }
-    
-
-    
 }
